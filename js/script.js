6f90a712b9b3ff9cfa74238fd3c644f231c84fcd
Drew <drew@finstrom.us>
2015-05-26 09:22:46 -0700

    fixed issues with bad merge

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 "use strict";
-<<<<<<< HEAD
 
-
-=======
->>>>>>> b15f126d10083a9096e4a8c4df4100f80da40464
 //jQuery
 $( document ).ready(function() {
     console.log( "jQuery Ready" );
@@ -16,36 +12,7 @@ var foo = function(bar) {
   return false;
 };
 
-<<<<<<< HEAD
+
 var triangle = function(sideA, sideB, sideC) {
   return 'invalid';
 };
-=======
-
-
-var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-$(document).ready(function() {
-  $("#result").hide();
-  $("form#leap-year").submit(function(event) {
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-
-    $(".year").text(year);
-    if (!result) {
-      $(".not").text("not");
-    } else {
-      $(".not").text("");
-    }
-
-    $("#result").show();
-    event.preventDefault();
-  });
-});
->>>>>>> b15f126d10083a9096e4a8c4df4100f80da40464
--- server.sh
+++ server.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 python -m SimpleHTTPServer 4567
-=======
-sudo python -m SimpleHTTPServer 88
->>>>>>> b15f126d10083a9096e4a8c4df4100f80da40464
